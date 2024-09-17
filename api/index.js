4c34eef56288cbda35fffaeb56d0c2ecf8e75c95
genr234 <80691876+genr234@users.noreply.github.com>
2024-09-17 21:55:45 +0200

    Update index.js

--- api/index.js
+++ api/index.js
@@ -1,7 +1,6 @@
 const express = require('express');
 const app = express();
 const path = require('path');
-const axios = require('axios')
 
 
 //settings
@@ -18,18 +17,3 @@ app.listen((app.get('port')), function () {
     console.log('listening on 80')
 })
 
-const url = `doki-doki.genr234.com`; 
-const interval = 30000;
-
-function reloadWebsite() {
-  axios.get(url)
-    .then(response => {
-      console.log(`Reloaded at ${new Date().toISOString()}: Status Code ${response.status}`);
-    })
-    .catch(error => {
-      console.error(`Error reloading at ${new Date().toISOString()}:`, error.message);
-    });
-}
-
-
-setInterval(reloadWebsite, interval);
